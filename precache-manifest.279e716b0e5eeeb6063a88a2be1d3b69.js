8b98587071aade201580b9539f83a5a15a8fa65e
Barney Gammond <barney@gammond.co.uk>
2020-09-02 18:28:35 +0100

    Updates

---
+++ precache-manifest.279e716b0e5eeeb6063a88a2be1d3b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c136c4f83e89bd4b01a6abfb5ff1c626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7c8b58c145e2a1bf39",
+    "url": "/static/css/main.463cf1af.chunk.css"
+  },
+  {
+    "revision": "2f03cf92805ddd7b1f24",
+    "url": "/static/js/2.ee2732d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee2732d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7c8b58c145e2a1bf39",
+    "url": "/static/js/main.060a0866.chunk.js"
+  },
+  {
+    "revision": "24819105bc2b5bd6f130",
+    "url": "/static/js/runtime-main.726d328e.js"
+  }
+]);
